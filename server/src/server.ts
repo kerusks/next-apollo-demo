3d146a9c72351797ff9d2e63ecbfcd38f6242457
kerusks <kerusks@gmail.com>
2021-09-13 14:42:32 +0100

    update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,8 @@ import cors from 'cors';
 
 const startApolloServer = async (schema: GraphQLSchema) => {
   const port = process.env.PORT || 5000
-  const app = express();
+  const app = express();  
+  app.set('port', port)
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
     schema,
@@ -16,7 +17,7 @@ const startApolloServer = async (schema: GraphQLSchema) => {
   });
   await server.start();
   server.applyMiddleware({ app });
-  await new Promise(resolve => httpServer.listen({ port }, resolve()));
+  await new Promise(resolve => httpServer.listen(resolve));
   console.log(`Graphql Server started on: http://localhost:${port}${server.graphqlPath}`)
 }
 
